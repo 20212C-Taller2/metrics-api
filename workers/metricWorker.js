5b0f2c264e1d6f4fabdce922fe400effe7715545
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2021-12-11 19:07:47 -0300

    Changed metrics worker db connection order.

--- workers/metricWorker.js
+++ workers/metricWorker.js
@@ -3,41 +3,40 @@ const config = require("./../config/config");
 const queue = config.METRICS_QUEUE;
 const database = require("./../model/Database.js");
 const logger = require("../services/log/logService");
-
-
 const Metric = require("./../model/schema/metric");
-const {error} = require("../services/log/logService");
 
-async function processServicesMetrics() {
+function processServicesMetrics() {
   logger.log(`Starting processing metrics`);
-  const connection = await amqp.connect(config.CLOUDAMQP_URL);
-  const channel = await connection.createChannel();
-  await channel.assertQueue(queue);
-  channel.consume(queue, async (message) => {
-    const content = JSON.parse(message.content.toString());
-    logger.log(`Received message from "${queue}" queue: `, JSON.stringify(content));
-    let metric = new Metric({
-      service: content.service,
-      operation: content.operation,
-      date: content.date,
+  database
+    .connect()
+    .then(async () => {
+      const connection = await amqp.connect(config.CLOUDAMQP_URL);
+      const channel = await connection.createChannel();
+      await channel.assertQueue(queue);
+      channel.consume(queue, async (message) => {
+        const content = JSON.parse(message.content.toString());
+        logger.log(`Received message from "${queue}" queue: `, JSON.stringify(content));
+        let metric = new Metric({
+          service: content.service,
+          operation: content.operation,
+          date: content.date,
+        });
+        try {
+          await metric.save();
+          channel.ack(message);
+        } catch (error) {
+          logger.error("Cannot save metrics: ", JSON.stringify(content));
+          logger.error(error);
+        }
+      });
+      setTimeout(() => {
+        channel.close();
+        connection.close();
+      }, 500);
+    })
+    .catch((error) => {
+      logger.error("Error in metrics worker: ", error);
     });
-    try {
-      await metric.save();
-      channel.ack(message);
-    } catch (error) {
-      logger.error("Cannot save metrics: ",JSON.stringify(content))
-      logger.error(error)
-    }
-    setTimeout(() => {
-      channel.close();
-      connection.close();
-    }, 500);
-  });
 }
 
-database.connect().then(() => {
-  processServicesMetrics()
-}).catch((error) => {
-  logger.error("Error in metrics worker: ", error)
-})
-
+processServicesMetrics();
